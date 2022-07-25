3d77cb7905c5813a54cb24179d021b6df261edfa
EdsonFufu <edsonfufu@NMB-HQ101-MAC046.local>
2022-07-26 01:30:11 +0300

    first commit

---
+++ src/main/java/com/simplilearn/project/icinonlinebackapp/IcinOnlineBackAppApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.project.icinonlinebackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcinOnlineBackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IcinOnlineBackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplilearn/project/icinonlinebackapp/IcinOnlineBackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.project.icinonlinebackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcinOnlineBackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
